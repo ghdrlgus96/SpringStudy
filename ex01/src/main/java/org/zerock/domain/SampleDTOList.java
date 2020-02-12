3fb4bab3c997282d053cd9c5f57e85bad33ccb93
Hong <ghdrlgus96@naver.com>
2020-02-12 16:34:18 +0900

    Spring Study 20200212

---
+++ ex01/src/main/java/org/zerock/domain/SampleDTOList.java
@@ -0,0 +1,12 @@
+package org.zerock.domain;
+
+import java.util.*;
+import lombok.Data;
+
+public class SampleDTOList {
+	private List<SampleDTO> list;
+	
+	public SampleDTOList() {
+		list = new ArrayList<>();
+	}
+}
